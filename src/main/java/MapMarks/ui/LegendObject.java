c37faaec3d43d41785ee9b2b6945841ff970dc04
casey <406326-casey@users.noreply.gitlab.com>
2021-09-28 03:42:30 -0400

    Help Tooltip

--- src/main/java/MapMarks/ui/LegendObject.java
+++ src/main/java/MapMarks/ui/LegendObject.java
@@ -5,6 +5,8 @@ import MapMarks.utils.ColorEnum;
 import MapMarks.utils.MapMarksTextureDatabase;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.helpers.TipHelper;
 import com.megacrit.cardcrawl.helpers.input.InputHelper;
 import easel.ui.AbstractWidget;
 import easel.ui.AnchorPosition;
@@ -18,6 +20,12 @@ import java.util.function.Consumer;
 public class LegendObject extends AbstractWidget<LegendObject> {
     private static final float WIDTH = 70;
     private static final float HEIGHT = 70;
+
+    public static final String TIP_HEADER = "Map Marks: Controls";
+    public static final String TIP_BODY = "#gRight+Click a node to toggle its highlight. #gRight+Click+Drag to toggle many nodes at once. #gRight+Click the legend items to toggle all of a certain type. #gRight+Click this legend button to clear all highlights. NL NL #pRight+Click+Drag outside nodes to open a radial menu to select the color. NL NL #rALT+Right+Click to start painting with the current color. #rALT+Right+Click this legend button to clear all pen drawings. NL NL #bControl+Right+Click a node to clear all highlights from nodes unreachable to it (i.e. no path between another previously highlighted node and the current target).";
+//    public static final String TIP_BODY = "#gRight+Click this legend button to clear all highlights. #gRight+Click a node to toggle highlights. #gRight+Click and drag to highlight/unhighlight many nodes at once. #gRight+Click outside nodes to open a radial menu to select the color. #gRight+Click the legend items (e.g. Elites, Treasure, etc.) to toggle all nodes of a certain type. NL NL #rALT+Right+Click to start painting with the current color. #rALT+Right+Click this legend button to clear all pen drawings. NL NL #bControl+Right+Click a node to clear all highlights from nodes unreachable to it (i.e. no path between another previously highlighted node and the current target). NL NL Note: Unreachable nodes due to pathing choices are hidden by default; hovering over this legend button will reveal highlighted but unreachable nodes.";
+    public static final String TIP_HEADER_SUCCINCT = "Map Marks";
+    public static final String TIP_BODY_SUCCINCT = "Hold #gShift to show instructions and controls for this mod.";
 
     private static Color SHADOW_COLOR = Color.valueOf("94abb2ff");
 
@@ -44,6 +52,28 @@ public class LegendObject extends AbstractWidget<LegendObject> {
 
         this.onMouseEnter(me -> me.setDimHighlight(INVIS_COLOR, HIGHLIGHT_COLOR));
         this.onMouseLeave(me -> me.setDimHighlight(INVIS_COLOR, INVIS_COLOR));
+    }
+
+    @Override
+    protected void updateWidget() {
+        super.updateWidget();
+        if (hb.hovered) {
+            if (EaselInputHelper.isShiftPressed()) {
+                TipHelper.renderGenericTip(1500.0f * Settings.xScale,
+                        (getBottom() - 20.0f) * Settings.scale,
+                        //270.0f * Settings.scale,
+                        TIP_HEADER,
+                        TIP_BODY);
+            }
+            else {
+                TipHelper.renderGenericTip(1500.0f * Settings.xScale,
+//                        (getBottom() - 20.0f) * Settings.scale,
+                        270.0f * Settings.scale,
+                        TIP_HEADER_SUCCINCT,
+                        TIP_BODY_SUCCINCT);
+            }
+        }
+
     }
 
     private void setDimHighlight(Color dim, Color highlight) {
